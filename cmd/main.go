3c22c010d6ee9c53bdfd36e3047bd2e1efc74504
emirrcaglar <emirrcaglar@gmail.com>
2025-07-23 11:16:52 +0300

    db conn + env

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/emirrcaglar/go-restapi/cmd/api"
+	"github.com/emirrcaglar/go-restapi/config"
+	"github.com/emirrcaglar/go-restapi/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal("error loading config: ", err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal("error running api server: ", err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal("error pinging database: ", err)
+	}
+	log.Println("db: successfully connected.")
 }
