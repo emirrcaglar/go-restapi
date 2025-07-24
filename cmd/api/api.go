e7cf13a23231cd4e8d82f563e9ef44d8a08eed20
emirrcaglar <emirrcaglar@gmail.com>
2025-07-24 14:35:49 +0300

    fix migration + add get products

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/emirrcaglar/go-restapi/service/product"
 	"github.com/emirrcaglar/go-restapi/service/user"
 	"github.com/gorilla/mux"
 )
@@ -28,6 +29,10 @@ func (s *APIServer) Run() error {
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("listening on ", s.addr)
 
